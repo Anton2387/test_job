2dded2a82f3b3115bbb792edc9ccc46d0f8e5181
Anton23 <antonavdonin87@gmail.com>
2022-12-11 23:56:03 +0300

    формируем массив из строк, которые меньше и равны 3 символам

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+string[] array = new string[5]{ "23", "hello", "d34", "friends", "qwerty"};
+Console.WriteLine("First");
+PrintArray(array);
+
+Console.WriteLine();
+
+string[] newArray = new string[array.Length];
+Console.WriteLine("Second");
+NewArray(array, newArray);
+PrintArray(newArray);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void NewArray(string[] array, string[] newArray)
+{
+    for (var i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3 )
+        {
+            newArray[i] = array[i];
+        }
+    }
+}
